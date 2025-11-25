c771aaa4ded1fe93eb32fdd96b924ecfcb902539
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-25 12:27:59 +0100

    add last correction

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+
 const port = 3000;
 
+
+app.use(express.json());
+
+
 const postsRouter = require("./routers/posts");
+
 
 app.use("/posts", postsRouter);
 
--- routers/posts.js
+++ routers/posts.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+
 const posts = require("../posts");
+
+router.get("/", (req, res) => {
+    res.send("Lista dei post");
+});
 
 router.get("/:id", (req, res) => {
     const { id } = req.params;
