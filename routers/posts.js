2940963aebe795ca5fe4ecd8ded494d0ce270bfa
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-25 11:52:33 +0100

    ad the router.get  for to take the informaction

--- routers/posts.js
+++ routers/posts.js
@@ -2,3 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const posts = require("../posts");
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    res.send(`Dettagli del post ${id}`);
+});
