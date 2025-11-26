ea8f1b8081478c6c859ab76c3291463daafd0b60
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-26 09:55:16 +0100

    add bonus

--- routers/posts.js
+++ routers/posts.js
@@ -5,6 +5,21 @@ const router = express.Router();
 const posts = require("../posts");
 
 router.get("/", (req, res) => {
+    res.json(posts);
+});
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    const post = posts.find(p => p.id === parseInt(id));
+
+    if (!post) {
+        return res.status(404).json({ error: "Post non trovato" });
+    }
+
+    res.json(post);
+});
+
+router.get("/", (req, res) => {
     res.send("Lista dei post");
 });
 
